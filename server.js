c4d46bb26efea2d411d0228f6ed5ae79a2ba0311
baggersgere <matt.bagnall33@gmail.com>
2021-10-07 14:01:45 +0100

    new source

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 const corsOptions = {
   exposedHeaders: "authorization",
   credentials: true,
-  origin: "http://localhost:3000",
+  origin: "https://create-react-app-games.herokuapp.com/",
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
 };
 app.use(cors(corsOptions));
