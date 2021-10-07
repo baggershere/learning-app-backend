430b1607fffc65f69b732245d627f539e6d8bbc5
baggersgere <matt.bagnall33@gmail.com>
2021-10-07 14:42:18 +0100

    cors

--- server.js
+++ server.js
@@ -28,21 +28,12 @@ app.use(cookieParser());
 
 app.use(express.json());
 const corsOptions = {
-  exposedHeaders: "authorization",
-  //credentials: true,
+  credentials: true,
+  origin: true,
   //origin: "https://create-react-app-games.herokuapp.com/",
-  methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
 };
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(cors(corsOptions));
-app.options("*", cors());
 
 app.get("/testroute", (req, res) => {
   res.send("test route");
